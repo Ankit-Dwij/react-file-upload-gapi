da4944d3bcd2038c5a6f1668abe5120c85c95044
Ankit Dwij <ankitdwiz010@gmail.com>
2022-06-10 13:55:41 +0530

    server endpoint created

--- server/server.js
+++ server/server.js
@@ -5,6 +5,25 @@ const app = express();
 
 app.use(fileUpload());
 
+//Upload Endpoint
+
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file uploaded" });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/../../public/uploads/${file.name}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
 app.listen(5000, () => {
   console.log("server started...");
 });
